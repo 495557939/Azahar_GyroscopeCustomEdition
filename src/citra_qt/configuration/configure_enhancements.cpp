3be5807e71e82b999bc3e01ae1fddf9a83f4a4da
495557939 <495557939@QQ.COM>
2026-06-25 04:16:59 +0800

    bugfix

--- src/citra_qt/configuration/configure_enhancements.cpp
+++ src/citra_qt/configuration/configure_enhancements.cpp
@@ -263,11 +263,8 @@ void ConfigureEnhancements::ApplyConfiguration() {
         Settings::values.pp_shader_name =
             ui->shader_combobox->itemText(ui->shader_combobox->currentIndex()).toStdString();
         auto save_filter_slot = [&](int idx, Settings::Setting<std::string>& setting) {
-            if (idx >= 0 && idx < 9 && filter_slots[idx]) {
-                if (filter_slots[idx]->isVisible())
-                    setting.SetValue(filter_slots[idx]->currentText().toStdString());
-                // Hidden slots: keep whatever value was already set (don't force "None")
-            }
+            if (idx >= 0 && idx < 9 && filter_slots[idx])
+                setting.SetValue(filter_slots[idx]->currentText().toStdString());
         };
         save_filter_slot(0, Settings::values.pp_shader_name_2);
         save_filter_slot(1, Settings::values.pp_shader_name_3);
